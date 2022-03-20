6a0e05a46d0f837c650f18a05d792464f8e65a72
SeaWorld <robert.klitsch97@gmail.com>
2022-03-20 15:41:58 -0700

    Added Jest config

---
+++ test-suite.config.example.js
@@ -0,0 +1,12 @@
+// To use this configuration file, rename it to test-suite.config.js
+// and fill it with your own configuration.
+// NamebaseJS will expect either a Session, or both a Key(aKey) and Secret(sKey).
+// Without these filled out, most tests will fail, other than public endpoints.
+
+const namebasejs = new (require('../namebasejs'))({
+    Session: '', // Expects a valid session cookie
+    aKey: false, // sKey is also needed, expects string
+    sKey: false, // aKey is also needed, expects string
+});
+
+module.exports = namebasejs;
