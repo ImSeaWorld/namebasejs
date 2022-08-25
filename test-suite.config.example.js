67ad7211f13e7609874377c8d2dfc361340a7289
SeaWorld <robert.klitsch97@gmail.com>
2022-08-24 19:11:46 -0700

    Converted from CommonJS to ES module

--- test-suite.config.example.js
+++ test-suite.config.example.js
@@ -2,9 +2,10 @@
 // and fill it with your own configuration.
 // NamebaseJS will expect either a Session, or both a Key(aKey) and Secret(sKey).
 // Without these filled out, most tests will fail, other than public endpoints.
+import NameBase from './namebasejs/index.js';
 
-const namebasejs = new (require('../namebasejs'))({
-    Session: '', // Expects a valid session cookie
+const namebasejs = new NameBase({
+    session: '', // Expects a valid session cookie
     aKey: false, // sKey is also needed, expects string
     sKey: false, // aKey is also needed, expects string
 });
